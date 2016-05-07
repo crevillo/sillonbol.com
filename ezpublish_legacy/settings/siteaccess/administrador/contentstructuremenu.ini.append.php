35e69cedf6e8bfd8bbf9dd7f3308d9d2c0fe7431
crevillo <crevillo@gmail.com>
2016-05-07 12:03:58 +0200

    legacy settings

---
+++ ezpublish_legacy/settings/siteaccess/administrador/contentstructuremenu.ini.append.php
@@ -0,0 +1,13 @@
+<?php /* #?ini charset="utf-8"?
+
+[TreeMenu]
+Dynamic=enabled
+ShowClasses[]
+ShowClasses[]=folder
+ShowClasses[]=user_group
+ShowClasses[]=documentation_page
+ShowClasses[]=event_calender
+ShowClasses[]=frontpage
+ShowClasses[]=forums
+ShowClasses[]=gallery
+*/ ?>
