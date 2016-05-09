b16af48d143e9cc35324b406890489e9e092d615
crevillo <crevillo@gmail.com>
2016-05-09 22:05:06 +0200

    fixed sitemap

--- src/Sillonbol/WebBundle/Controller/SiteMapController.php
+++ src/Sillonbol/WebBundle/Controller/SiteMapController.php
@@ -18,16 +18,16 @@ use eZ\Publish\API\Repository\Values\Content\Query\SortClause;
 class SiteMapController extends Controller
 {
 
-    public function siteMapAction( Request $request )
+    public function siteMapAction(Request $request)
     {
-	
-        $includeCriterion = $this->get('sillonbol.criteria_helper')
-                ->generateContentTypeIncludeCriterion(
-                // Get contentType identifiers we want to include from configuration (see default_settings.yml).
-                $this->container->getParameter('sillonbol.top_menu.content_types_include')
-        );
 
-        $contentList = $this->get('sillonbol.menu_helper')->getTopMenuContent( 2, $includeCriterion );   
+        $includeCriterion = $this->get('sillonbol.criteria_helper')
+            ->generateContentTypeIncludeCriterion(
+            // Get contentType identifiers we want to include from configuration (see default_settings.yml).
+                $this->container->getParameter('sillonbol.top_menu.content_types_include')
+            );
+
+        $contentList = $this->get('sillonbol.menu_helper')->getTopMenuContent(2, $includeCriterion);
 
         $menuList = array();
         // Looping against search results to build $locationList
@@ -37,46 +37,46 @@ class SiteMapController extends Controller
             $menuList[$contentId] = $this->getRepository()->getLocationService()->loadLocation($content->contentInfo->mainLocationId);
         }
 
-	$location = $this->getRepository()->getLocationService()->loadLocation( 2 );
+        $location = $this->getRepository()->getLocationService()->loadLocation(2);
         $query = new Query();
 
         $query->limit = 200;
 
         $query->query = new Criterion\LogicalAnd(
             array(
-                new Criterion\Visibility( Criterion\Visibility::VISIBLE ),
-                new Criterion\Subtree( $location->pathString ),
+                new Criterion\Visibility(Criterion\Visibility::VISIBLE),
+                new Criterion\Subtree($location->pathString),
                 new Criterion\ContentTypeIdentifier(
-                    array( 'article' )
+                    array('article')
                 )
             )
         );
 
-        $query->sortClauses = array( new SortClause\DateModified( Query::SORT_DESC ) );
+        $query->sortClauses = array(new SortClause\DateModified(Query::SORT_DESC));
 
-        $articleList = $this->getRepository()->getSearchService()->findContent( $query )->searchHits;
+        $articleList = $this->getRepository()->getSearchService()->findContent($query)->searchHits;
 
-	$articles = array();
+        $articles = array();
 
-	foreach ($articleList as $contentId => $article) {
+        foreach ($articleList as $contentId => $article) {
             $content = $article->valueObject;
             $articles[$contentId] = $this->getRepository()->getLocationService()->loadLocation($content->contentInfo->mainLocationId);
         }
 
 
-	$response = $this->render(
-            'SillonbolWebBundle:sitemap:sitemap.xml.twig',
+        $response = $this->render(
+            ':sitemap:sitemap.xml.twig',
             array(
-               'home' => $this->getRepository()->getContentService()->loadContent(57),
-               'menuList' => $menuList,
-	       'articles' => $articles
+                'home' => $this->getRepository()->getContentService()->loadContent(57),
+                'menuList' => $menuList,
+                'articles' => $articles
             )
         );
 
         $response->setPublic();
-        $response->setMaxAge( 86400 );
-        $response->setSharedMaxAge( 86400 );
-        $response->headers->set( 'X-Location-Id', 2 );
+        $response->setMaxAge(86400);
+        $response->setSharedMaxAge(86400);
+        $response->headers->set('X-Location-Id', 2);
 
         return $response;
     }
